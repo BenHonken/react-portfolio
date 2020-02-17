a97de246b19af352741c2ff4154a88d879b2acb5
Ben Honken <honkenb@gmail.com>
2020-02-17 14:40:30 -0600

    updated contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,30 +1,43 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
-import Learn from "./Learn";
 
 function Contact(props) {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis molestie urna.
-        Aliquam semper ultrices varius. Aliquam faucibus sit amet magna a ultrices. Aenean
-        pellentesque placerat lacus imperdiet efficitur. In felis nisl, luctus non ante euismod,
-        tincidunt bibendum mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque nec
-        metus vestibulum, egestas massa eu, sollicitudin ipsum. Nulla facilisi. Sed ut erat ligula.
-        Nam tincidunt nunc in nibh dictum ullamcorper. Class aptent taciti sociosqu ad litora
-        torquent per conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus.
-        Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-      <Link to={`${props.match.url}/learn`} role="button" className="btn btn-link">
-        Learn More
-      </Link>{" "}
-      <Link to="/contact" role="button" className="btn btn-link">
-        Learn Less
-      </Link>
-      <Route exact path={`${props.match.url}/learn`} component={Learn} />
-    </div>
+    <div class="pushFooter">
+            <div class="container wrapper">
+                <h1>
+                    Contact
+                </h1>
+                <hr />
+                <div class="row">
+                    <div class="col-md-6 contactBottom">
+                        <div>Phone:</div>
+                        <br />
+                        <div>(320) 212 - 6815</div>
+                        <br />
+                    </div>
+                    <div class="col-md-6 contactBottom">
+                        <div>Email:</div>
+                        <br />
+                        <div>
+                            <a class="bottomLinks" href="mailto:honkenb@gmail.com">honkenb@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div class="row contactBottom">
+                    <div class="col-md-4 contactBottom">
+                        <a class="bottomLinks" href={require("../../HonkenResume.pdf")} target="blank">Resume</a>
+                    </div>
+                    <div class="col-md-4 contactMid">
+                        <a class="bottomLinks" href="https://github.com/BenHonken" target="blank">Github</a>
+                    </div>
+                    <div class="col-md-4  contactBottom">
+                        <a class="bottomLinks" href="https://www.linkedin.com/in/ben-honken-b1b07818a/" target="blank">LinkedIn</a>
+                    </div>
+                </div>
+            </div>
+        </div>
   );
 }
 
