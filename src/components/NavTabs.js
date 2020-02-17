2ae536e80f5ed4bd2a064c992f98e67398e3791b
Ben Honken <honkenb@gmail.com>
2020-02-17 14:39:21 -0600

    updated nav tabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,45 +3,37 @@ import { Link } from "react-router-dom";
 
 function NavTabs() {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/about"
-          className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/blog"
-          className={window.location.pathname === "/blog" ? "nav-link active" : "nav-link"}
-        >
-          Blog
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact"
-          className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact/learn"
-          className={window.location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"}
-        >
-          Learn
-        </Link>
-      </li>
-    </ul>
+  <nav class="navbar navbar-expand navbar-light bg-light">
+    <div class="navwrapper">
+        <div class="navbar-brand" id="name" >Ben Honken</div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbuttoncontainer">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                      <a class="nav-link navbutton" href="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"} >About <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li>
+                      <div class="vr"></div>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link navbutton" href="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</a>
+                  </li>
+                  <li>
+                      <div class="vr"></div>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link navbutton" href="/contact" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                      Contact
+                      </a>
+                  </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  </nav>
   );
 }
 
