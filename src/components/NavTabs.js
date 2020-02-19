a8c1e526e46ae839e2f4ea1b09c2e586d089a591
Ben Honken <honkenb@gmail.com>
2020-02-19 13:42:52 -0600

    updated routes

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -13,19 +13,19 @@ function NavTabs() {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                   <li class="nav-item active">
-                      <a class="nav-link navbutton" href="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"} >About <span class="sr-only">(current)</span></a>
+                      <Link class="nav-link navbutton" to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"} >About <span class="sr-only">(current)</span></a>
                   </li>
                   <li>
                       <div class="vr"></div>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link navbutton" href="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</a>
+                      <Link class="nav-link navbutton" to="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</a>
                   </li>
                   <li>
                       <div class="vr"></div>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link navbutton" href="/contact" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                      <Link class="nav-link navbutton" to="/contact" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
                       Contact
                       </a>
                   </li>
