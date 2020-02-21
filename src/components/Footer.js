232ad69722b8b1047ff736a2316cd3830d3617c8
Ben Honken <honkenb@gmail.com>
2020-02-20 22:05:57 -0600

    created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer>Copyright Ben Honken 2020, all rights reserved</footer>
+  );
+}
+
+export default Footer;
